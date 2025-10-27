6ea9fc99ca8aed8a866435e70c230dd86fe8fd2f
Katherine Harvey <kharvey@college.harvard.edu>
2025-10-26 21:37:39 -0400

    NUTRI-23: add environment variable debugging

--- api/auth/login.js
+++ api/auth/login.js
@@ -70,6 +70,12 @@ module.exports = async (req, res) => {
   }
 
   try {
+    // Debug environment variables
+    console.log('Environment check:');
+    console.log('FIREBASE_PROJECT_ID:', process.env.FIREBASE_PROJECT_ID ? 'SET' : 'NOT SET');
+    console.log('FIREBASE_CLIENT_EMAIL:', process.env.FIREBASE_CLIENT_EMAIL ? 'SET' : 'NOT SET');
+    console.log('FIREBASE_PRIVATE_KEY:', process.env.FIREBASE_PRIVATE_KEY ? 'SET' : 'NOT SET');
+    console.log('FIREBASE_API_KEY:', process.env.FIREBASE_API_KEY ? 'SET' : 'NOT SET');
     const { email, password } = req.body;
 
     // Validate input
