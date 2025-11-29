55f470fcfdafbcabe180074788986c4ea7832eb7
Waseem Ahmad <yourpersonal@gmail.com>
2025-11-28 23:16:40 -0500

    NUTRI-119: Update page styles to use theme variables

--- frontend/src/pages/Auth.js
+++ frontend/src/pages/Auth.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useAuth } from '../context/AuthContext';
 import CustomSelect from '../components/CustomSelect';
+import ThemeToggle from '../components/ThemeToggle';
 import './Auth.css';
 
 const Auth = () => {
@@ -424,6 +425,7 @@ const Auth = () => {
 
   return (
     <div className="auth-container">
+      <ThemeToggle />
       <div className="auth-background">
         <div className="auth-blob blob-1"></div>
         <div className="auth-blob blob-2"></div>
